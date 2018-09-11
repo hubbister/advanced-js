0c5ef663d03159e0b96cd3d4bd4a139bef81451d
Hubbister <serventi.lorenzo@gmail.com>
2018-09-11 20:43:26 +0200

    da casa

--- script.js
+++ script.js
@@ -6,116 +6,141 @@ var john = {
     yearOfBirth: 1990,
     job: 'teacher'
 };
-var Person = function(name, yearOfBirth, job) {
+
+// function constructor
+var Person = function(name, yearOfBirth,job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
 }
-Person.prototype.calculateAge  = function() {
+
+Person.prototype.calculateAge = function() {
     console.log(2016 - this.yearOfBirth);
 };
+
+
 Person.prototype.lastName = 'Smith';
-var john = new Person('John', 1990, 'teacher');
+
+var john = new Person('John', 1990, 'Teacher');
+
+
 var jane = new Person('Jane', 1969, 'designer');
 var mark = new Person('Mark', 1948, 'retired');
+
 john.calculateAge();
 jane.calculateAge();
-mark.calculateAge();
+mark.calculateAge(); 
+
+
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
 */
-
-
-
 /////////////////////////////
 // Lecture: Object.create
 /*
-var personProto = {
+var personProto  = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
     }
 };
+
 var john = Object.create(personProto);
 john.name = 'John';
 john.yearOfBirth = 1990;
 john.job = 'teacher';
+
 var jane = Object.create(personProto, {
     name: { value: 'Jane' },
     yearOfBirth: { value: 1969 },
-    job: { value: 'designer' }
-});
-*/
+    job: { value: 'designer' },
+})*/
+
 
 
 
 /////////////////////////////
 // Lecture: Primitives vs objects
 /*
-// Primitives
+//primitives
 var a = 23;
 var b = a;
 a = 46;
-console.log(a);
-console.log(b);
-// Objects
+
+console.log(a,b);
+
+// object
 var obj1 = {
     name: 'John',
     age: 26
 };
+
+//nuova referenza che punta allo stesso
+//oggetto
 var obj2 = obj1;
 obj1.age = 30;
+
 console.log(obj1.age);
 console.log(obj2.age);
-// Functions
+
+// functions
 var age = 27;
 var obj = {
     name: 'Jonas',
     city: 'Lisbon'
 };
+
 function change(a, b) {
-    a = 30;
+    a = 30; //non si modifica perchè è una primitiva
     b.city = 'San Francisco';
 }
-change(age, obj);
-console.log(age);
-console.log(obj.city);
-*/
 
+change(age,obj);
 
+console.log(age,obj);*/
 
 /////////////////////////////
 // Lecture: Passing functions as arguments
+
 /*
 var years = [1990, 1965, 1937, 2005, 1998];
-function arrayCalc(arr, fn) {
+
+function arrayCalc(arr, fn){
+
     var arrRes = [];
-    for (var i = 0; i < arr.length; i++) {
+    
+    for (var i = 0; i < arr.length; i++){
         arrRes.push(fn(arr[i]));
     }
     return arrRes;
 }
+
 function calculateAge(el) {
     return 2016 - el;
 }
+
+
 function isFullAge(el) {
     return el >= 18;
 }
-function maxHeartRate(el) {
-    if (el >= 18 && el <= 81) {
+
+function maxHeartRate(el){
+    if (el >= 18 && el <=81){
         return Math.round(206.9 - (0.67 * el));
-    } else {
-        return -1;
+    }
+    else{
+        return - 1;
     }
 }
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var rates = arrayCalc(ages, maxHeartRate);
+
+var ages = arrayCalc(years,calculateAge);
+var fullJapan = arrayCalc(ages,isFullAge); 
+var rates = arrayCalc(ages,maxHeartRate);
+
 console.log(ages);
+console.log(fullJapan);
 console.log(rates);
 */
-
-
 
 /////////////////////////////
 // Lecture: Functions returning functions
@@ -123,49 +148,50 @@ console.log(rates);
 function interviewQuestion(job) {
     if (job === 'designer') {
         return function(name) {
-            console.log(name + ', can you please explain what UX design is?');
+            console.log(name + ', can you please explain what UX design is this?');
         }
     } else if (job === 'teacher') {
         return function(name) {
-            console.log('What subject do you teach, ' + name + '?');
+            console.log('wath subject do you teach ' + name + '?');
         }
     } else {
-        return function(name) {
-            console.log('Hello ' + name + ', what do you do?');
+        return function (name) {
+            console.log('Hello' + name + ',wath do you do?');
         }
     }
 }
+
 var teacherQuestion = interviewQuestion('teacher');
+//console.log(teacherQuestion);
+teacherQuestion('mary');
+
 var designerQuestion = interviewQuestion('designer');
-teacherQuestion('John');
 designerQuestion('John');
-designerQuestion('jane');
-designerQuestion('Mark');
-designerQuestion('Mike');
-interviewQuestion('teacher')('Mark');
+designerQuestion('Jane');
+
+
+interviewQuestion('teacher')('Marco');
 */
-
-
-
 /////////////////////////////
 // Lecture: IIFE
-/*
+
+
 function game() {
     var score = Math.random() * 10;
     console.log(score >= 5);
 }
 game();
+
+
 (function () {
     var score = Math.random() * 10;
     console.log(score >= 5);
 })();
-//console.log(score);
+
 (function (goodLuck) {
     var score = Math.random() * 10;
-    console.log(score >= 5 - goodLuck);
+    console.log(score >= 5 -goodLuck);
 })(5);
-*/
-
 
 
 /////////////////////////////
